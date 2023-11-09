517aae20a17663cdcec4cd1d77e0df1a4d614617
dosaa7 <dosaa7@naver.com>
2023-11-09 14:25:37 +0900

    판넬 제출

--- src/pages/dashboard/myProblemSet.js
+++ src/pages/dashboard/myProblemSet.js
@@ -4,12 +4,13 @@ import MyProblemList from './myProblemList';
 
 const MyProblemSet = ({ problemList }) => (
   <div>
-    <h1>1학년이라면 필수!</h1>
+    <h1 style={{display: "inline"}}>1학년 추천 </h1><h3 style={{display: "inline"}}>made by mango</h3>
+    <p><h2 style={{display: "inline"}}>80문제| </h2><h4 style={{display: "inline"}}>56hits!</h4></p>
     <Box
       sx={{
         mt: 5,
         width: 1,
-        height: 320,
+        height: 620,
         borderRadius: 2,
         bgcolor: (theme) => '#ECECEC',
         border: (theme) => `dashed 1px ${theme.palette.divider}`,
